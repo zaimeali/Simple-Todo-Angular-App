8787125fca07390538fa9cfd64f9a653d207ffd9
Zaime Ali <zaimeali1997@gmail.com>
2020-05-15 21:00:31 +0500

    Simple To Do Done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,48 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'simple-todo-app';
+  title = 'My Todo List';
+
+  addTodo(newTodoLabel: string){
+    var newTodo = {
+      label: newTodoLabel,
+      priority: 1,
+      done: false
+    };
+
+    this.todos.push(newTodo);
+  }
+
+  clickTodo(todo){
+    this.todos = this.todos.filter(
+      t => t.label !== todo.label
+    );
+  }
+
+  changeDone(todo){
+    if(todo.done){
+      todo.done = false;
+    }
+    else{
+      todo.done = true;
+    }
+  }
+  
+  todos = [
+    {
+      label: 'Bring Milk',
+      done: false,
+      priority: 3
+    },
+    {
+      label: 'Bring Water',
+      done: true,
+      priority: 4
+    },
+    {
+      label: 'Bring Book',
+      done: false,
+      priority: 1
+    }
+  ];
 }
